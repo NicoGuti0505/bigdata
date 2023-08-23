5600b10e71da37230812ec47740420f09e4efb6c
NicoGuti0505 <nicolastolim1@gmail.com>
2023-08-22 21:27:23 -0500

    script

--- prueba.py
+++ prueba.py
@@ -1,5 +1,19 @@
-import scrip
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    if numero <= 3:
+        return True
+    if numero % 2 == 0 or numero % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= numero:
+        if numero % i == 0 or numero % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
 
 
 def test_es_primo():
-    assert scrip.es_primo(4)
+    assert es_primo(4)
