9c2d6a18ff66985815b509cace79bc27a7dc787a
Nicolas <nicolastolim1@gmail.com>
2023-08-31 10:25:00 -0500

    Update todo

---
+++ extraer.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import datetime
+import boto3
+
+def main():
+    url_add = "https://www.eltiempo.com/"
+    response = requests.get(url_add)
+    html_content = response.text
+    fecha = datetime.datetime.now().strftime("%Y-%m-%d")
+    pagina =  f'news/raw/contenido-{fecha}.html'
+
+    bucket_name = "parcialbg"
+
+    s3 = boto3.client('s3')
+    s3.put_object(Body=html_content , Bucket=bucket_name, Key=pagina)
+
+    print(f"Archivo '{pagina}' subido a '{bucket_name}'")
+
+
+main()
+
